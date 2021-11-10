29fe0c87fab7c8fb3fb62add6ce1ad4af7713642
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-11-10 17:52:41 -0500

    updated the footer

--- src/components/PortfolioContent.js
+++ src/components/PortfolioContent.js
@@ -29,13 +29,13 @@ export default function PortfolioContent() {
             <div className="container-fluid footer-container bg-secondary text-white">
                 <div className="row d-flex justify-content-around">
                     <div className="col-4 text-center">
-                        Email
+                    <a href="https://www.github.com/markcirineo" target="_blank" rel="noreferrer" className="a-footer" ><i className="fab fa-github"></i></a>
                     </div>
                     <div className="col-4 text-center">
-                        GitHub
+                    <a href="mailto:markcirineo22@gmail.com" className="a-footer" ><i className="fas fa-envelope"></i></a>
                     </div>
                     <div className="col-4 text-center">
-                        LinkedIn
+                        <a href="https://www.linkedin.com/in/mark-cirineo/" target="_blank" rel="noreferrer" className="a-footer"><i className="fab fa-linkedin"></i></a>
                     </div>
                 </div>
             </div>
