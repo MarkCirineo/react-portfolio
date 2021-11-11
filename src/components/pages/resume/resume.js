b009d4df50691e6237c7511c392ccab066869395
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-11-10 23:35:57 -0500

    potentional fix to resume download

--- src/components/pages/resume/resume.js
+++ src/components/pages/resume/resume.js
@@ -8,7 +8,7 @@ export default function Resume() {
             <div className="row col-10 col-xl-12">
                 <div className="d-flex align-items-center flex-column">
                     <h1>Resume</h1>
-                    <a href="/react-portfolio/resume.pdf" to="/react-portfolio/resume.pdf" target="_blank" rel="noreferrer" className="resume" download>Download my resume!</a>
+                    <a href="%PUBLIC_URL%/resume.pdf" to="%PUBLIC_URL%/resume.pdf" target="_blank" rel="noreferrer" className="resume" download>Download my resume!</a>
                 </div>
                 <div className="container">
                     <div className="row d-flex justify-content-center">
