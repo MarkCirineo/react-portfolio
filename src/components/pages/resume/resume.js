5de43f6a0b7b8575f5c3d4dc58b3e5ee1bb09706
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-11-11 00:17:01 -0500

    possible fix to resume download

--- src/components/pages/resume/resume.js
+++ src/components/pages/resume/resume.js
@@ -8,7 +8,7 @@ export default function Resume() {
             <div className="row col-10 col-xl-12">
                 <div className="d-flex align-items-center flex-column">
                     <h1>Resume</h1>
-                    <a href="/resume.pdf" target="_blank" rel="noreferrer" className="resume" download>Download my resume!</a>
+                    <a href="/resume.2706fc47.pdf" target="_blank" rel="noreferrer" className="resume" download>Download my resume!</a>
                 </div>
                 <div className="container">
                     <div className="row d-flex justify-content-center">
