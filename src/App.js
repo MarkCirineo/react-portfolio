77f62538dfd42812bfdc477882f4870ef15c2264
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-11-15 19:03:02 -0500

    refactored to use HashRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from "react";
-import PortfolioContent from "./components/PortfolioContent";
+import React, { useState } from "react";
+import NavTabs from "./components/NavTabs";
+import Footer from "./components/Footer";
+import AboutMe from "./components/pages/about-me/about-me";
+import ContactMe from "./components/pages/contact-me/contact-me";
+import Portfolio from "./components/pages/portfolio/portfolio";
+import Resume from "./components/pages/resume/resume";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <div>
-      <PortfolioContent/>
-    </div>
-  );
+    const [currentPage, setCurrentPage] = useState("About Me");
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div className="parent">
+            <HashRouter>
+                <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+                <Routes>
+                    <Route path="/about-me" element={ <AboutMe /> } />
+                    <Route path="/portfolio" element={ <Portfolio /> } />
+                    <Route path="/contact-me" element={ <ContactMe /> } />
+                    <Route path="/resume" element={ <Resume /> } />
+                </Routes>
+                <Footer />
+            </HashRouter>
+        </div>
+    );
 }
 
 export default App;
--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../index.css"
 
 function NavTabs(props) {
@@ -7,34 +8,34 @@ function NavTabs(props) {
             <div className="row align-items-center d-flex flex-wrap-reverse justify-content-lg-around justify-content-md-center text-center">
                 <div className="col-lg-3 col-md-5 header-name d-flex flex-column"><span className="name-span">Mark Cirineo</span> <span className="desc-span">Full-stack web developer</span></div>
                 <div className="col-lg-6 col-md-12 d-flex justify-content-around flex-nowrap header-links">
-                    <a 
-                        href="#about-me" 
+                    <Link
+                        to="/about-me"
                         onClick={() => props.handlePageChange("About Me")}
                         className={props.currentPage === "About Me" ? "a-header active" : "a-header"}
                     >
                         About Me
-                    </a>
-                    <a 
-                        href="#portfolio" 
+                    </Link>
+                    <Link
+                        to="/portfolio"
                         onClick={() => props.handlePageChange("Portfolio")}
                         className={props.currentPage === "Portfolio" ? "a-header active" : "a-header"}
                     >
                         Portfolio
-                    </a>
-                    <a 
-                        href="#contact-me" 
+                    </Link>
+                    <Link
+                        to="/contact-me"
                         onClick={() => props.handlePageChange("Contact Me")}
                         className={props.currentPage === "Contact Me" ? "a-header active" : "a-header"}
                     >
                         Contact Me
-                    </a>
-                    <a 
-                        href="#resume" 
+                    </Link>
+                    <Link
+                        to="/resume"
                         onClick={() => props.handlePageChange("Resume")}
                         className={props.currentPage === "Resume" ? "a-header active" : "a-header"}
                     >
                         Resume
-                    </a>
+                    </Link>
                 </div>
             </div>
         </header>
