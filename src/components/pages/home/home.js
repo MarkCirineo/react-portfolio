59cc16e3b20fb749a4471e8a4a592a357c7ff38e
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-11-16 20:34:58 -0500

    updated styling and order for links

--- src/components/pages/home/home.js
+++ src/components/pages/home/home.js
@@ -10,11 +10,11 @@ function Home() {
                 <h2 className="home-h2">I'm a Full-stack web developer.</h2>
             </div>
             <div className="home-link-div col-lg-8 col-md-10 col-sm-11 col-xs-12 d-flex align-items-center justify-content-center flex-columns container-fluid">
-                <div className="d-flex justify-content-center row flex-column container-fluid align-items-center">
-                    <a className="link col-md-8 col-sm-10 col-xs-12 text-center" href="mailto:markcirineo">Send me an email at markcirineo22@gmail.com</a>
-                    <a className="link col-md-8 col-sm-10 col-xs-12 text-center" href="https://www.linkedin.com/in/mark-cirineo/" target="_blank" rel="noreferrer">Visit my LinkedIn</a>
-                    <a className="link col-md-8 col-sm-10 col-xs-12 text-center" href="https://www.github.com/markcirineo/" target="_blank" rel="noreferrer">Visit my GitHub</a>
-                    <Link className="link col-md-8 col-sm-10 col-xs-12 text-center" to="/portfolio">View My Work</Link>
+                <div className="d-flex justify-content-center flex-column container-fluid align-items-center">
+                    <Link className="link col-lg-7 col-md-8 col-sm-10 col-xs-12 text-center" to="/portfolio">View My Work</Link>
+                    <a className="link col-lg-7 col-md-8 col-sm-10 col-xs-12 text-center" href="https://www.github.com/markcirineo/" target="_blank" rel="noreferrer">Visit my GitHub</a>
+                    <a className="link col-lg-7 col-md-8 col-sm-10 col-xs-12 text-center" href="https://www.linkedin.com/in/mark-cirineo/" target="_blank" rel="noreferrer">Visit my LinkedIn</a>
+                    <a className="link col-lg-7 col-md-8 col-sm-10 col-xs-12 text-center" href="mailto:markcirineo">Send me an email at markcirineo22@gmail.com</a>
                 </div>
             </div>
         </div>
