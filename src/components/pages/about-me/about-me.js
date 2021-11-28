e75f32b3ca8890ee425672254f690fd8b6fd66dc
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-11-28 15:46:09 -0500

    edited bootstrap styling and classes for custom styles

--- src/components/pages/about-me/about-me.js
+++ src/components/pages/about-me/about-me.js
@@ -5,18 +5,22 @@ import "./about-me.css"
 export default function AboutMe() {
 
     return (
-        <div className="container-fluid content-container">
-            <div className="row d-flex justify-content-center">
-                <div className="col-lg-4 col-md-8 col-sm-9 col-xs-12 justify-content-center">
-                    <h1 className="about-me-h1">About Me</h1>
-                    <img alt="me" src={mark} className="mark" />
-                </div>
-                <div className="col-lg-8 col-md-12 d-flex align-items-center">
-                    <p className="about-me-p">
-                        My name is Mark. I am a Full-stack web developer with a certificate from the University of Pennsylvania. 
-                        I'm proficient in HTML, CSS, JavaScript, MySQL, and MongoDB. I enjoy working with technologies including Node.js, Express.js, and ReactJS.
-                        I also have experience working with Sequelize, Mongoose, jQuery, Jest, Handlebars, Bootstrap and Materialize.
-                    </p>
+        <div>
+            <div className="container-fluid d-flex justify-content-center bg-secondary col-sm-6 col-md-4 col-xs-12 col-12 h1-parent">
+                <h1 className="about-me-h1">About Me</h1>
+            </div>
+            <div className="container-fluid content-container about-me-parent d-flex">
+                <div className="row d-flex justify-content-evenly">
+                    <div className="col-lg-3 col-md-8 col-sm-9 col-xs-12 justify-content-center bg-secondary image-div">
+                        <img alt="me" src={mark} className="mark-img" />
+                    </div>
+                    <div className="col-lg-7 col-sm-11 d-flex align-items-center text-center bg-secondary description-div">
+                        <p className="about-me-p">
+                            My name is Mark. I am a Full-stack web developer with a certificate from the University of Pennsylvania. 
+                            I'm proficient in HTML, CSS, JavaScript, MySQL, and MongoDB. I enjoy working with technologies including Node.js, Express.js, and ReactJS.
+                            I also have experience working with Sequelize, Mongoose, jQuery, Jest, Handlebars, Bootstrap and Materialize.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
