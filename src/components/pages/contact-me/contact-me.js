27d9e7ea3773b6579fe0c753eab623a9fc5f5f36
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-11-30 16:26:27 -0500

    added bootstrap styling to the input section of the form

--- src/components/pages/contact-me/contact-me.js
+++ src/components/pages/contact-me/contact-me.js
@@ -39,79 +39,82 @@ export default function ContactMe() {
     }
 
     return (
-        <div className="container pt-4">
-            <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-                <Alert dismissible onClose={() => setShowSuccessAlert(false)} show={showSuccessAlert} variant="success">
-                    Email sent!
-                </Alert>
-                <Alert dismissible onClose={() => setShowErrorAlert(false)} show={showErrorAlert} variant="danger">
-                    Something went wrong! Click <a href="mailto:markcirineo22@gmail.com">here</a> to try a different method.
-                </Alert>
-                <Form.Group>
-                    <Form.Label htmlFor="name">
-                        Name
-                    </Form.Label>
-                    <Form.Control 
-                        type="text"
-                        placeholder="Your name"
-                        name="name"
-                        onChange={handleInputChange}
-                        value={userFormData.name}
-                        required
-                    />
-                </Form.Group>
-                <Form.Control.Feedback type="invalid">
-                    Name is required!
-                </Form.Control.Feedback>
+        <div className="container pt-4 d-flex justify-content-center flex-column align-items-center">
+            
+            <div className="col-sm-12 col-md-9 col-12 contact-me-div bg-secondary">
+                <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+                    <Alert dismissible onClose={() => setShowSuccessAlert(false)} show={showSuccessAlert} variant="success">
+                        Email sent!
+                    </Alert>
+                    <Alert dismissible onClose={() => setShowErrorAlert(false)} show={showErrorAlert} variant="danger">
+                        Something went wrong! Click <a href="mailto:markcirineo22@gmail.com">here</a> to try a different method.
+                    </Alert>
+                    <Form.Group>
+                        <Form.Label htmlFor="name">
+                            Name
+                        </Form.Label>
+                        <Form.Control 
+                            type="text"
+                            placeholder="Your name"
+                            name="name"
+                            onChange={handleInputChange}
+                            value={userFormData.name}
+                            required
+                        />
+                    </Form.Group>
+                    <Form.Control.Feedback type="invalid">
+                        Name is required!
+                    </Form.Control.Feedback>
 
-                <Form.Group>
-                    <Form.Label htmlFor="email">
-                        Email
-                    </Form.Label>
-                    <Form.Control 
-                        type="email"
-                        placeholder="Your email address"
-                        name="email"
-                        onChange={handleInputChange}
-                        value={userFormData.email}
-                        required
-                    />
-                </Form.Group>
-                <Alert dismissible onClose={() => setShowInvalidEmailAlert(false)} show={showInvalidEmailAlert} variant="danger">
-                    Invalid Email address!
-                </Alert>
-                <Form.Control.Feedback type="invalid">
-                    Email is required!
-                </Form.Control.Feedback>
+                    <Form.Group>
+                        <Form.Label htmlFor="email">
+                            Email
+                        </Form.Label>
+                        <Form.Control 
+                            type="email"
+                            placeholder="Your email address"
+                            name="email"
+                            onChange={handleInputChange}
+                            value={userFormData.email}
+                            required
+                        />
+                    </Form.Group>
+                    <Alert dismissible onClose={() => setShowInvalidEmailAlert(false)} show={showInvalidEmailAlert} variant="danger">
+                        Invalid Email address!
+                    </Alert>
+                    <Form.Control.Feedback type="invalid">
+                        Email is required!
+                    </Form.Control.Feedback>
 
-                <Form.Group>
-                    <Form.Label htmlFor="message">
-                        Message
-                    </Form.Label>
-                    <Form.Control 
-                        type="text"
-                        as="textarea"
-                        rows={4}
-                        placeholder="Your message"
-                        name="message"
-                        onChange={handleInputChange}
-                        value={userFormData.message}
-                        required
-                    />
-                </Form.Group>
-                <Form.Control.Feedback type="invalid">
-                    Message is required!
-                </Form.Control.Feedback>
+                    <Form.Group>
+                        <Form.Label htmlFor="message">
+                            Message
+                        </Form.Label>
+                        <Form.Control 
+                            type="text"
+                            as="textarea"
+                            rows={4}
+                            placeholder="Your message"
+                            name="message"
+                            onChange={handleInputChange}
+                            value={userFormData.message}
+                            required
+                        />
+                    </Form.Group>
+                    <Form.Control.Feedback type="invalid">
+                        Message is required!
+                    </Form.Control.Feedback>
 
-                <Button
-                    disabled={!(userFormData.name && userFormData.email && userFormData.message)}
-                    type="submit"
-                    variant="success"
-                    className="bg-white text-dark"
-                >
-                    Submit
-                </Button>
-            </Form>
+                    <Button
+                        disabled={!(userFormData.name && userFormData.email && userFormData.message)}
+                        type="submit"
+                        variant="success"
+                        className="bg-white text-dark"
+                    >
+                        Submit
+                    </Button>
+                </Form>
+            </div>
         </div>
     )
 }
