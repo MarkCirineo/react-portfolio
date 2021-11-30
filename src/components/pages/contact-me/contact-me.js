732981334cd09af1e837517f96eb43e90d9aa1dc
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-11-30 14:26:34 -0500

    refactored form to use react-bootstrap

--- src/components/pages/contact-me/contact-me.js
+++ src/components/pages/contact-me/contact-me.js
@@ -1,28 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
 import "./contact-me.css"
 
 export default function ContactMe() {
 
+    const [userFormData, setUserFormData] = useState({ name: "", email: "", message: "" });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setUserFormData({ ...userFormData, [name]: value});
+    }
+    
     return (
         <div className="container pt-4">
-            <div>
-                <h4>The best way to contant me is by sending an email to <a href="mailto:markcirineo22@gmail.com">markcirineo22@gmail.com</a></h4>
-            </div>
-            <div className="mb-3 mt-3">
-                <label htmlFor="name-input" className="form-label">Name</label>
-                <input type="text" className="form-control" id="name-input" placeholder="name"/>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="email-input" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="email-input" placeholder="name@example.com"/>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="message-input" className="form-label">Message</label>
-                <textarea className="form-control" id="message-input" rows="4"></textarea>
-            </div>
-            <div className="col-auto">
-                <button type="submit" className="btn mb-3"><a className="button-a" href="mailto:markcirineo@gmail.com">Send</a></button>
-            </div>
+            <Form>
+                <Form.Group>
+                    <Form.Label htmlFor="name">
+                        Name
+                    </Form.Label>
+                    <Form.Control 
+                        type="text"
+                        placeholder="Your name"
+                        name="name"
+                        onChange={handleInputChange}
+                        value={userFormData.name}
+                        required
+                    />
+                </Form.Group>
+                <Form.Control.Feedback type="invalid">
+                    Name is required!
+                </Form.Control.Feedback>
+
+                <Form.Group>
+                    <Form.Label htmlFor="email">
+                        Email
+                    </Form.Label>
+                    <Form.Control 
+                        type="email"
+                        placeholder="Your email address"
+                        name="email"
+                        onChange={handleInputChange}
+                        value={userFormData.email}
+                        required
+                    />
+                </Form.Group>
+                <Form.Control.Feedback type="invalid">
+                    Email is required!
+                </Form.Control.Feedback>
+
+                <Form.Group>
+                    <Form.Label htmlFor="message">
+                        Message
+                    </Form.Label>
+                    <Form.Control 
+                        type="text"
+                        as="textarea"
+                        rows={4}
+                        placeholder="Your message"
+                        name="message"
+                        onChange={handleInputChange}
+                        value={userFormData.message}
+                        required
+                    />
+                </Form.Group>
+                <Form.Control.Feedback type="invalid">
+                    Message is required!
+                </Form.Control.Feedback>
+
+                <Button
+                    disabled={!(userFormData.name && userFormData.email && userFormData.message)}
+                    type="submit"
+                    variant="success"
+                    className="bg-white text-dark"
+                >
+                    Submit
+                </Button>
+            </Form>
         </div>
     )
 }
